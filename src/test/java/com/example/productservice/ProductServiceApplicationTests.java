676d880b3b5a5d6905d2f6cddf129cff43c4abd2
prem <premtsd@gmail.com>
2024-01-20 21:14:03 +0530

    laptop change

--- src/test/java/com/example/productservice/ProductServiceApplicationTests.java
+++ src/test/java/com/example/productservice/ProductServiceApplicationTests.java
@@ -1,13 +1,19 @@
 package com.example.productservice;
 
+import com.example.productservice.models.Product;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class ProductServiceApplicationTests {
 
     @Test
     void contextLoads() {
+        List<Product> findByTitle(){
+
+        }
     }
 
 }
